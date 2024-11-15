1301ef6d4fd5c6a764d4f60fcc4767b99cc288a5
hendryar <115210134+hendryar@users.noreply.github.com>
2024-11-15 12:55:21 +0800

    fix fix fix again for delete esp from pond

--- controllers/esp32.controller.js
+++ controllers/esp32.controller.js
@@ -4,90 +4,9 @@ import { CreateSuccess } from "../utils/success.js";
 import { CreateError } from "../utils/error.js";
 import { createAlert } from "./alert.controller.js";
 import jwt from "jsonwebtoken";
-//v2
-// export const addReadingToDatabase = (req, res) => {
-//     const { serialNumber, passKey, phReading, heightReading, temperatureReading, tdsReading } = req.body;
-  
-//     // Try to find a pond with matching serial number and passkey.
-//     Pond.findOne({ connectedEsp32Serial: serialNumber, connectedEsp32Passkey: passKey })
-//       .then(pond => {
-//         if (!pond) {
-//           // If pond is not found, return 404 error.
-//           // The readings will not be processed nor added into the database.
-//           return res.status(404).json(CreateError(404, "Pond not found or incorrect serial number/passkey."));
-//         }
-  
-//         const alarmings = [];
-//         // Check if the parameters are within the safe range.
-//         if (pond.safeMinPh > phReading || pond.safeMaxPh < phReading) {
-//           alarmings.push("ph");
-//         }
-//         if (pond.safeMinTemperature > temperatureReading || pond.safeMaxTemperature < temperatureReading) {
-//           alarmings.push("temperature");
-//         }
-//         if (pond.safeMinHeight > heightReading || pond.safeMaxHeight < heightReading) {
-//           alarmings.push("height");
-//         }
-//         if (pond.safeMinTds > tdsReading || pond.safeMaxTds < tdsReading) {
-//           alarmings.push("tds");
-//         }
-  
-//         // If a pond is found, proceed with creating the ESP32 reading.
-//         // The pondId is also associated with the ESP32 reading here.
-//         const esp32 = new Esp32({
-//           serialNumber,
-//           passKey,
-//           phReading,
-//           heightReading,
-//           temperatureReading,
-//           tdsReading,
-//           pondId: pond._id
-//         });
-//         console.log("esp32 data: ", esp32);
-  
-//         // The created ESP32 reading will then be saved into the database.
-//         esp32.save()
-//           .then(data => {
-//             // Emit the new reading to connected clients
-//             const pondId = pond._id.toString(); // Ensure pondId is a string
-  
-//             // Access the io instance
-//             const io = req.app.get('socketio');
-  
-//             // Emit the new reading to the pond room
-//             io.to(pondId).emit('new-reading', data);
-  
-//             // If there are any alarming parameters, emit an alert
-//             if (alarmings.length > 0) {
-//               io.to(pondId).emit('alert', {
-//                 pondId: pondId,
-//                 message: 'Reading out of safe range',
-//                 parameters: alarmings,
-//                 reading: data
-//               });
-//             }
-  
-//             console.log("Saving reading");
-//             return res.status(201).json(CreateSuccess(201, "Esp32 reading created successfully!", data));
-//           })
-//           .catch(err => {
-//             console.log("err diatas: ", err);
-//             return res.status(601).json(CreateError(601, "Some error occurred while creating the Esp32 reading.", err));
-//           });
-//       })
-//       .catch(err => {
-//         console.log("error adding reading: ", err);
-//         return res.status(500).json(CreateError(500, "Some error occurred while retrieving the Pond.", err));
-//       });
-//   };
-  
-
-//Deletes all reading that has the pondId in the req body
-//This is used when a pond is deleted, all readings associated with the pond should also be deleted.
-//This is to prevent orphaned readings in the database.
 
 
-//v3
+
 export const addReadingToDatabase = (req, res) => {
     const { serialNumber, passKey, phReading, heightReading, temperatureReading, tdsReading } = req.body;
 
@@ -151,8 +70,6 @@ export const addReadingToDatabase = (req, res) => {
         });
 };
 
-
-
 export const deleteReadingByPondId = async (req, res) => {
     const pondId = req.query.pondId;
 
@@ -176,11 +93,6 @@ export const deleteReadingByPondId = async (req, res) => {
         const pond = await Pond.findById(pondId);
         if (!pond) {
             return res.status(404).json(CreateError(404, "Pond not found"));
-        }
-
-        const esp32Readings = await Esp32.find({ pondId: pondId });
-        if (esp32Readings.length === 0) {
-            return res.status(404).json(CreateError(404, "No readings found with the pondId provided."));
         }
 
         await Esp32.deleteMany({ pondId: pondId });
